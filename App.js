527888eef2b8e6ee5ab9f697b4343ed76b1175ed
julianarce1995 <100883327+julianarce1995@users.noreply.github.com>
2023-04-20 21:04:54 -0500

    dsa
    asd

--- App.js
+++ App.js
@@ -1,15 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons'
+import Contacts from './screens/Contacts';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator 
+      ialRouteName='Home'
+      >
+        <Stack.Screen name='HomeTabs' component={HomeTabs}  options={{title:'sistema de prueba'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{marginBottom: 20}}>Inicio de Sesion</Text>
     </View>
   );
 }
 
+function ProductScreen({navigation}) {
+  let title='Clarines'
+  let name='Contactalo'
+  return (
+    <View style={styles.container}>
+      <Text>Estamos en Productos</Text>
+      <Button
+      title="Ir a Contacts"
+      onPress={()=>{
+        navigation.navigate('Contacts', {title:title, name:name})
+      }}
+      >
+      </Button>
+    </View>
+  );
+}
+
+function HomeTabs(){
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown:false,
+        tabBarActiveTintColor:'red',
+        tabBarInactiveTintColor:'gray',
+        tabBarActiveBackgroundColor:'aqua',
+        tabBarInactiveBackgroundColor:'powderblue'
+      }}
+    >
+      <Tab.Screen name='Home' component={HomeScreen} options={{
+        tabBarIcon:(tabInfo)=>(<MaterialIcons name="person"/>)
+      }}></Tab.Screen>
+      <Tab.Screen name='Products' component={ProductScreen}></Tab.Screen>
+      <Tab.Screen name='Contacts' component={Contacts}></Tab.Screen>
+    </Tab.Navigator>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
