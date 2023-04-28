b2d57d3483dc961a2914a237a9e91c036b39d134
julianarce1995 <100883327+julianarce1995@users.noreply.github.com>
2023-04-27 19:37:54 -0500

    resultado final
    clase para enviar proyecto final

--- App.js
+++ App.js
@@ -1,69 +1,112 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button} from 'react-native-paper';
+// Importar componentes para la navegación y generación de la pila de screens
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons} from '@expo/vector-icons';
 import Contacts from './screens/Contacts';
+import { useState } from 'react';
+let users = [
+  {email:'hruiz@gmail.com',name:'Humberto Ruiz',password:'11', role:1},
+  {email:'jdoe@gmail.com',name:'John Doe',password:'22', role:2}
+]
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-      ialRouteName='Home'
+      <Stack.Navigator
+        initialRouteName='HomeTabs'
       >
-        <Stack.Screen name='HomeTabs' component={HomeTabs}  options={{title:'sistema de prueba'}}></Stack.Screen>
+        <Stack.Screen name='HomeTabs' component={HomeTabs} options={{title:'Sistema Prueba'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function HomeScreen({navigation}) {
-  return (
+function HomeScreen({navigation}){
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errormess, setErrormess] = useState('')
+  return(
     <View style={styles.container}>
-      <Text style={{marginBottom: 20}}>Inicio de Sesion</Text>
+      <Text style={{marginBottom:20}}>Inicio de Sesión</Text>
+      <TextInput 
+      label='Correo Electronico'
+      mode='outline'
+      left={<TextInput.Icon icon='account'/>}
+      onChangeText={email => setEmail(email)}
+      value={email}
+      />
+      <TextInput 
+      label='Contraseña'
+      mode='outline'
+      right={<TextInput.Icon icon='eye'/>}
+      onChangeText={password => setPassword(password)}
+      value={password}
+      secureTextEntry
+      />
+      <Button
+        icon='login'
+        mode='contained'
+        onPress={()=>{
+          let findUser = users.find(usr => usr.email == email && usr.password == password);
+          if (findUser != undefined) {
+            const {name, email} = findUser
+            setErrormess('')
+            setEmail('')
+            setPassword('')
+            navigation.navigate('Contacts',{name:name, email:email})
+          }else{
+            setErrormess('Correo y/o contraseña incorrecto ')
+          }
+        }}
+      >
+        Inicio Sesión
+      </Button>
+      <Text>{errormess}</Text>
     </View>
   );
 }
 
-function ProductScreen({navigation}) {
-  let title='Clarines'
-  let name='Contactalo'
-  return (
+function ProductsScreen({navigation}){
+  let title = 'Este es el titulo'
+  let fullname = 'Pepito Perez'
+  return(
     <View style={styles.container}>
-      <Text>Estamos en Productos</Text>
-      <Button
-      title="Ir a Contacts"
-      onPress={()=>{
-        navigation.navigate('Contacts', {title:title, name:name})
-      }}
-      >
-      </Button>
+      <Text style={{marginBottom:10}}>Estamos en Productos</Text>
     </View>
   );
 }
 
 function HomeTabs(){
-  return (
+  return(
     <Tab.Navigator
       screenOptions={{
         headerShown:false,
         tabBarActiveTintColor:'red',
         tabBarInactiveTintColor:'gray',
-        tabBarActiveBackgroundColor:'aqua',
+        tabBarActiveBackgroundColor:'orange',
         tabBarInactiveBackgroundColor:'powderblue'
       }}
     >
       <Tab.Screen name='Home' component={HomeScreen} options={{
-        tabBarIcon:(tabInfo)=>(<MaterialIcons name="person"/>)
-      }}></Tab.Screen>
-      <Tab.Screen name='Products' component={ProductScreen}></Tab.Screen>
-      <Tab.Screen name='Contacts' component={Contacts}></Tab.Screen>
+        tabBarStyle:{display:'none'},
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='home' size={22}/>)
+      }}/>
+      <Tab.Screen name='Products' component={ProductsScreen} options={{
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='apps' size={22}/>)
+      }}/>
+      <Tab.Screen name='Contacts' component={Contacts} options={{
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='chat' size={22}/>)
+      }}/>
     </Tab.Navigator>
-  )
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
